db8ad99d5e8c541ff2f6cd5802c49480e9eabb70
Ellie-Cunningham <elliecunningham627@gmail.com>
2025-07-21 16:17:30 -0400

    Initialization process complete. Memory and registers appear to be in correct default state upon boot.

---
+++ CHIP8.h
@@ -0,0 +1,26 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+class CHIP8 {
+  private:
+    unsigned char RAM[4096];
+    unsigned char V[16]; // CPU registers formaly named V0 - VE with the final register representing a 'carry flag'.
+    unsigned short I; // Index register
+    unsigned short pc; // Program counter
+    unsigned short currentOpcode;
+
+    // When calling a subroutine or jump command, the previous location is stored in a stack to be returned to.
+    unsigned short stack[16];
+    unsigned short stackPointer;
+
+    unsigned char graphicOutput[64*32]; // 64x32 resolution monochrome display.
+    unsigned char keypadState[16]; // 4x4 keypad for user input.
+
+    // Both timers automatically tick down at 60hz when not 0
+    unsigned char delayTimer;
+    unsigned char soundTimer;
+
+  public:
+    void initialization();
+    void CPUCycle();
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "CHIP8.h"
+
+int main() {
+  CHIP8 Chip8;
+  Chip8.initialization();
+  
+  // Step 1: setup graphics and input systems
+
+  // Step 2: Initialize system and load program
+
+  // Step 3: Loop CPU cycles
+
+  return 0;
+}
